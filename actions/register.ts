b6382fa67b13b78111c973da35af277f99da209b
Roman Parazyan <romaparazyan@gmail.com>
2024-05-22 01:57:37 +0300

    Added prisma scheme

--- actions/register.ts
+++ actions/register.ts
@@ -1,16 +1,33 @@
-'use server'
-
+'use server';
+import { db } from '@/lib/db';
 import * as z from 'zod';
+import bcrypt from 'bcryptjs';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
-export const register = async (values:z.infer<typeof RegisterSchema>) => {
-    const validated = RegisterSchema.safeParse(values);
-    
-    if (!validated.success) {
-        return { error: 'Invalid fields!'}
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+    const validatedFields = RegisterSchema.safeParse(values);
+
+    if (!validatedFields.success) {
+        return { error: 'Invalid fields!' };
     }
 
-    return {success: 'Email sent!'};
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await getUserByEmail(email);
+
+    if (existingUser) {
+        return { error: 'Email already in use!' };
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+        },
+    })
+
+
+    return { success: 'User created!' };
 };
-
-
